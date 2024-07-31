e9d3ef9d0f7afdc6e5c39f9f37873f49721f739e
clab-to <73862233+clab-to@users.noreply.github.com>
2024-07-31 22:59:13 +0900

    スキルも含めた最適化、あとクラスにしたけど微妙

--- optimizer.py
+++ optimizer.py
@@ -21,81 +21,114 @@ class Solver_Ids(Enum):
     GLPK_MIXED_INTEGER_PROGRAMMING = "GLPK_MIXED_INTEGER_PROGRAMMING"
 
 
-def optimize(equipments: pd.DataFrame, result_length):
-    equipments_list = equipments.to_dict("list")
+class EquipmentOptimizer:
+    def __init__(self, equipments_list: dict, **kwarg):
+        # 装備組合せ作成回数の取得、デフォルト10個
+        self.create_answer_count: int = kwarg.get("create_answer_count", 10)
 
-    solver = pywraplp.Solver.CreateSolver(
-        Solver_Ids.SAT_INTEGER_PROGRAMMING.value,
-    )
-    row_number, _ = equipments.shape
-    use_flag = {
-        index: solver.BoolVar(f"rowindex_{index:04}")
-        for index in range(row_number)
-    }
+        # 要求されている制約の辞書を取得
+        self.constraints: dict = kwarg.get("constraints", {})
 
-    for key in ["over-slot1", "over-slot2", "over-slot3", "over-slot4"]:
-        solver.Add(
-            solver.Sum(
-                [
-                    use_flag[index] * equipments_list[key][index]
-                    for index in range(row_number)
-                ]
-            )
-            >= 0
-        )
-    for key in ["head", "torso", "arm", "wst", "leg"]:
-        solver.Add(
-            solver.Sum(
-                [
-                    use_flag[index] * equipments_list[key][index]
-                    for index in range(row_number)
-                ]
-            )
-            <= 1
-        )
+        # 装備/装飾品と紐づくステータスの辞書を取得
+        self.eq_list: dict = equipments_list
+        self.eq_count: int = len(list(equipments_list.values())[0])
 
-    solver.Maximize(
-        solver.Sum(
-            [
-                use_flag[index] * equipments_list["def"][index]
-                for index in range(row_number)
-            ]
-        )
-    )
+        # solver用意
+        solver_id = kwarg.get("solver_id", Solver_Ids.SAT_INTEGER_PROGRAMMING)
+        self.solver = self._create_solver(solver_id)
+        self.flags = {
+            index: self.solver.BoolVar(f"rowindex_{index:04}")
+            for index in range(self.eq_count)
+        }
 
-    for _ in range(result_length):
-        status = solver.Solve()
+    def _create_solver(self, solver_id):
+        return pywraplp.Solver.CreateSolver(solver_id.value)
 
-        if (
-            status == pywraplp.Solver.OPTIMAL
-            or status == pywraplp.Solver.FEASIBLE
-        ):
-            used_indexes = [
-                index
-                for index in range(row_number)
-                if use_flag[index].solution_value() > 0.5
-            ]
-            for index in range(row_number):
-                if use_flag[index].solution_value():
-                    print(equipments_list["name"][index])
-            print(
-                sum(
-                    equipments_list["def"][index]
-                    * use_flag[index].solution_value()
-                    for index in range(row_number)
+    def _sum_rule(self, key):
+        # すべての装備/装飾品に対して、
+        # その装備/装飾品を利用するかのフラグとカラム別の値の積のリストを作成する
+        is_use_flag_and_value = [
+            self.flags[i] * self.eq_list[key][i] for i in range(self.eq_count)
+        ]
+        sum_value_in_use_equipments = self.solver.Sum(is_use_flag_and_value)
+        return sum_value_in_use_equipments
+
+    def _create_rule(self):
+        # 条件追加
+        # 防御力が最大であることを最適化の目的とする
+        for key in ["def"]:
+            self.solver.Maximize(self._sum_rule(key))
+        # 各部位は全身1つずつのみ装備できる
+        for key in ["head", "torso", "arm", "wst", "leg"]:
+            self.solver.Add(self._sum_rule(key) <= 1)
+        # 各サイズの空きスロットは0以下にならない
+        for key in ["over-slot1", "over-slot2", "over-slot3", "over-slot4"]:
+            self.solver.Add(self._sum_rule(key) >= 0)
+        # スキルとか空きスロットとかの条件追加
+        for key, value in self.constraints.items():
+            self.solver.Add(self._sum_rule(key) >= value)
+
+    def execute(self):
+        if not set(self.constraints.keys()).issubset(set(self.eq_list.keys())):
+            return []
+
+        results = []
+        self._create_rule()
+        for _ in range(self.create_answer_count):
+            status = self.solver.Solve()
+
+            is_optimal = status == pywraplp.Solver.OPTIMAL
+            is_feasible = status == pywraplp.Solver.FEASIBLE
+
+            if is_optimal or is_feasible:
+                equipment_names, defence = self._get_optimize_result()
+                self._add_not_duplicate_rule()
+                results.append(
+                    {"equipment_name": equipment_names, "defence": defence}
                 )
-            )
 
-            solver.Add(
-                solver.Sum(
-                    [
-                        use_flag[index]
-                        for index in range(row_number)
-                        if index in used_indexes
-                    ]
-                )
-                <= len(used_indexes) - 1
-            )
-        else:
-            print("The problem does not have an optimal solution.")
-            break
+            else:
+                print("The problem does not have an optimal solution.")
+                break
+
+        return results
+
+    def _get_optimize_result(self):
+        used_indexes = [
+            index
+            for index in range(self.eq_count)
+            if self.flags[index].solution_value()
+        ]
+        equipment_names = [
+            self.eq_list["name"][index] for index in used_indexes
+        ]
+        defence = sum(self.eq_list["def"][index] for index in used_indexes)
+        return equipment_names, defence
+
+    def _add_not_duplicate_rule(self):
+        # 条件追加
+        # 一度解として利用した組合せは再度選択されない
+        used_indexes = [
+            index
+            for index in range(self.eq_count)
+            if self.flags[index].solution_value()
+        ]
+        used_items = [self.flags[index] for index in used_indexes]
+        self.solver.Add(self.solver.Sum(used_items) <= len(used_indexes) - 1)
+
+
+def optimize(
+    equipments: pd.DataFrame,
+    create_answer_count: int,
+    constraints: dict = {},
+):
+    # nanがあるとsolver.Sumの計算がうまくいかないのでfillnaでnanを0に置き換える
+    # 意味としては0であってるのでこれでいい
+    equipments_list = equipments.fillna(0).to_dict("list")
+    optimizer = EquipmentOptimizer(
+        equipments_list,
+        constraints=constraints,
+        create_answer_count=create_answer_count,
+    )
+    result = optimizer.execute()
+    print(result)
